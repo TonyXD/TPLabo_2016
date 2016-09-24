510a1cdc191a73070723c4f9f12a2dd88151eaad
Tony <Tony@Tony-PC>
2016-09-23 23:59:19 -0300

    Se corrige modelado de mesas.

---
+++ src/Main/Modelo/Clases/Mesa.java
@@ -0,0 +1,9 @@
+package Modelo.Clases;
+
+public class Mesa {
+
+	int Capacidad;
+	int IdMesa;
+	int IdMozoAsignado;
+	int TipoEstado;
+}
--- src/Main/Modelo/Interfaces/AccionesMesa.java
+++
@@ -1,9 +0,0 @@
-package Modelo.Interfaces;
-
-/**Indica la funcionalidad que debe tener una mesa.*/
-public interface AccionesMesa {
-
-	/**Devuelve la capacidad de la mesa.*/
-	int getCapacidad();
-	
-}
