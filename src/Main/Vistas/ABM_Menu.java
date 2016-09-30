803da156f0635843ce02777e3b71c0e2e9aa07f3
Tony <Tony@Tony-PC>
2016-09-29 23:27:40 -0300

    Se Agrega la ventana de alta de menús

---
+++ src/Main/Vistas/ABM_Menu.java
@@ -0,0 +1,156 @@
+package Main.Vistas;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JTable;
+import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
+import javax.swing.border.TitledBorder;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.UIManager;
+import java.awt.Color;
+import java.awt.Toolkit;
+
+public class ABM_Menu extends JFrame {
+
+	private JPanel contentPane;
+	private JTable table;
+	private JTable table_1;
+	private JTable table_2;
+	private JTable table2;
+	private JTable table3;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ABM_Menu frame = new ABM_Menu();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ABM_Menu() {
+		setForeground(Color.WHITE);
+		setIconImage(Toolkit.getDefaultToolkit().getImage(ABM_Menu.class.getResource("/Main/Imagenes/dishes-798316_1280.png")));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1218, 800);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(10, 11, 723, 725);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.setBounds(10, 11, 703, 349);
+		panel.add(tabbedPane);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		tabbedPane.addTab("Entrada", null, scrollPane, null);
+		
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null},
+			},
+			new String[] {
+				"New column", "New column", "New column"
+			}
+		));
+		scrollPane.setViewportView(table);
+		
+		JScrollPane scrollPane2 = new JScrollPane();
+		tabbedPane.addTab("Principal", null, scrollPane2, null);
+		
+		table2 = new JTable();
+		table2.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null},
+			},
+			new String[] {
+				"New column", "New column", "New column"
+			}
+		));
+		scrollPane2.setViewportView(table2);
+		
+		JScrollPane scrollPane3 = new JScrollPane();
+		tabbedPane.addTab("Postre", null, scrollPane3, null);
+		
+		table3 = new JTable();
+		table3.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null},
+			},
+			new String[] {
+				"New column", "New column", "New column"
+			}
+		));
+		scrollPane3.setViewportView(table3);
+		
+		
+		JTabbedPane tabbedPane_1 = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane_1.setBounds(10, 370, 703, 344);
+		panel.add(tabbedPane_1);
+		
+		JScrollPane scrollPane_1 = new JScrollPane();
+		tabbedPane_1.addTab("Bebidas", null, scrollPane_1, null);
+		
+		table_1 = new JTable();
+		table_1.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null},
+			},
+			new String[] {
+				"New column", "New column", "New column"
+			}
+		));
+		scrollPane_1.setViewportView(table_1);
+		
+		JButton btnNewButton = new JButton("<<");
+		btnNewButton.setBounds(743, 378, 89, 59);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton(">>");
+		btnNewButton_1.setBounds(743, 308, 89, 59);
+		contentPane.add(btnNewButton_1);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Menu", TitledBorder.CENTER, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		panel_1.setBounds(844, 11, 354, 732);
+		contentPane.add(panel_1);
+		panel_1.setLayout(null);
+		
+		JScrollPane scrollPane_2 = new JScrollPane();
+		scrollPane_2.setBounds(6, 16, 338, 705);
+		panel_1.add(scrollPane_2);
+		
+		table_2 = new JTable();
+		table_2.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null},
+			},
+			new String[] {
+				"Nombre", "Descripcion", "Precio"
+			}
+		));
+		scrollPane_2.setViewportView(table_2);
+	}
+}
